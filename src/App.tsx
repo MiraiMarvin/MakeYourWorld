deb7d4c064ab791435d4edd1e9ccf07691409a70
Marvin Clerc <marvin.clerc@edu.devinci.fr>
2025-01-07 11:46:28 +0100

    MYW: Setup Project

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import './app.scss'
+
+function App() {
+
+  return (
+    <>
+      <section id='header'>
+      </section>
+
+      <section id='title'>
+        <h2>“Votre cœur parle pour vous “</h2>
+      </section>
+
+      <section id='canvas'>
+        {/* <canvas id='canvas' width='800' height='600'>< */}
+      </section>
+
+      <section id='content'>
+        <div>
+          <h1>P.LATINIU.M</h1>
+          <p>like a cry.</p>
+          <p>Laylow present:</p>
+          <div>
+            <div>
+              <ul>
+                <li>"Prismes Brisés(ft chuck noris)"</li>
+                <li>"Lumière Noire"</li>
+                <li>"Venin dans la Veine"</li>
+                <li>"Echos d’Abîmes"</li>
+                <li>"Os et Métal(ft zemmour)"</li>
+                <li>"Le Silence des Ombres"</li>
+                <li>"Sang Numérique (ft Mr anderson)"</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>"Nuit de Chrome (ft damso)"</li>
+                <li>"Âmes Fracturées"</li>
+                <li>"Rêves Inversés"</li>
+                <li>"Cathédrale de Néons"</li>
+                <li>"Cendres et Reflets"</li>
+                <li>"Symphonie pour un Labyrinthe"</li>
+                <li>"la peau de l'étranger(ft marine le pen)"</li>
+              </ul>
+            </div>
+          </div>
+          <p>Disponible sur toutes les plateformes le  le  vendredi 19 juin 2025 a Minuit </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
